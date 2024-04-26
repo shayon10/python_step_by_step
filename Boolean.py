f4b759a34cbca6ca68a34c8ead05ef56dece6555
shayon <s.i.shayon@gmail.com>
2024-04-26 18:04:39 +0600

    commit

---
+++ Boolean.py
@@ -0,0 +1,10 @@
+#bool type data
+#bool=True
+bool1 = bool(int(input("Enter 0 or 1 for bool1: ")))
+bool2 = bool(int(input("Enter 0 or 1 for bool2: ")))
+
+print("bool1 and bool2:", bool1 and bool2)
+print("bool1 or bool2:", bool1 or bool2)
+print("not bool1:", not bool1)
+
+
---
+++ stringformaaat.py
@@ -0,0 +1,3 @@
+num1 = 20
+num2 = 30
+print(f"this is my number {num1 + num2}")
